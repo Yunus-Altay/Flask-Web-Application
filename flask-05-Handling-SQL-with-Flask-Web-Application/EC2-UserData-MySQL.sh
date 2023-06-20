2d92378f92704a3a2a9397d8ecddb078b4c8e8a2
Yunus-Altay <yunusaltay.nl@gmail.com>
2023-06-20 23:41:22 +0200

    Flask web app with database uploaded

---
+++ flask-05-Handling-SQL-with-Flask-Web-Application/EC2-UserData-MySQL.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+yum update -y
+pip3 install flask -y
+pip3 install flask-mysql
+pip3 install sqlalchemy
+pip3 install Flask-SQLAlchemy
+yum install git -y
+sleep 10
+git clone https://github.com/Yunus-Altay/Flask-Web-Application.git
+cd ./Flask-Web-Application/flask-05-Handling-SQL-with-Flask-Web-Application
+python3 app-with-mysql.py
